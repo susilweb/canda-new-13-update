1350194f1f6228bfe9e0ec3db87cf80981e3a20b
Sushil Behera <sbehera@mpgbis.com>
2023-03-20 20:56:01 +0530

    business feature new page update

---
+++ pages/business-central-features-and-functionality.js
@@ -0,0 +1,375 @@
+import React, { useState } from "react";
+import Head from 'next/head';
+import Image from "next/image";
+import ModalVideo from "react-modal-video";
+import Link from "next/link";
+const Finance = () => {
+  const [isOpen, setOpen] = useState(false);
+  const [open, setsOpen] = useState(false);
+
+  return (
+    <>
+      <Head>
+        <title>Dynamics 365 Business Central: Features and Functionality</title>
+        <meta
+          name="description"
+          content="Dynamics 365 Business Central is a unified platform with comprehensive features and functionality to manage financials, operations, sales, & customer service."
+        />
+        <link
+          rel="canonical"
+          href="https://www.dynamicssquare.ca/business-central-features-and-functionality/"
+        />
+        <meta name="robots" content="index,follow" />
+      </Head>
+
+      <ModalVideo
+        channel="youtube"
+        autoplay
+        isOpen={isOpen}
+        videoId="NhoeKnSp7IM"
+        onClose={() => setOpen(false)}
+      /> 
+
+      <section className="Solution-banner hero-1 hero">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6 mobile-1 align-self-center">
+              <h1>Dynamics 365 Business Central: Feature, Functionality, and Benefits</h1>
+              <p>Business Central is a cloud-based solution that offers you a comprehensive, end-to-end approach to seamlessly manage and streamline your core business functions and enhance productivity.</p>
+              <div>
+                <div className="text-center text-lg-start">
+                  <a
+                    data-bs-toggle="modal"
+                    href="#exampleModal"
+                    className="btn-get-started scrollto"
+                  >
+                    <span>Start a FREE Demo</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 align-self-center">
+              <Image
+                src="/img/BC-feature-banner.png"
+                alt="BC-feature-banner"
+                width={734}
+                height={517}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-8 col-md-8">
+                <header className="section-header">
+                <h2>What can Dynamics 365 Business Central do for you?</h2>
+                <p>Help You Unlock New Levels of Productivity & Customer Engagement with Breeze.</p>
+                <p>Discover an all-in-one business management solution with <b>Dynamics 365 Business Central</b> (formerly <Link href="/products/microsoft-dynamics-nav/"><a>Dynamics NAV</a></Link> or Navision). <b>D365 Business Central</b> offers different modules to tackle any business structure. The seamless integration with other <b>Microsoft cloud services</b> and customizable features with Power Apps, Power Automate, and <Link href="/products/microsoft-power-bi/"><a>Power BI</a></Link> make <b>Business Central</b> stand out.</p>
+                <p>Trusted by over 160k customers worldwide, it streamlines processes, automates tasks, and enhances <b>customer engagement</b>, <Link href="/products/microsoft-dynamics-365-finance-operations/"><a>managing finances & operations</a></Link>, sales, and customer service. Upgrade from entry-level accounting software or legacy ERP systems and revolutionize your business with <b>Dynamics 365 Business Central's</b> single, end-to-end solution.</p>
+              </header>
+            </div>
+          </div>
+        </div>
+      </section>
+
+     
+      <section id="pricing" className="pricing">
+        <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-lg-8 col-md-8">
+            <header className="section-header">
+              <h2>Pricing and Licensing for D365 Business Central </h2>
+              <p><Link href="/products/dynamics-365-business-central/"><a>Business Central</a></Link> offers flexible pricing and licensing options that nicely fit the needs of businesses of all sizes.</p>
+            </header>
+          </div>
+        </div>
+         
+          <div className="row gy-4 p-9 justify-content-center">
+            <div className="col-lg-5">
+              <div className="box box-22">
+                <h3>Business Central's Essentials License</h3>
+                <div className="price">
+                $89.60(CAD) <span>Per-User/Month</span>
+                </div>
+                <ul>
+                  <li className="text-center">
+                  Includes:Financial management, supply chain management, project management, and more.
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-lg-5">
+              <div className="box box-22">
+                <h3>Business Central's Premium License</h3>
+                <div className="price">
+                $128(CAD) <span>Per-User/Month</span>
+                </div>
+                <ul>
+                  <li className="text-center">
+                  Includes: Manufacturing & service management functionality in addition to what Essentials License offers. 
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+          <div className="col-lg-12 col-md-12">
+              <div className="price-btn d-flex justify-content-center">
+                <a
+                  data-bs-toggle="modal"
+                  href="#exampleModal"
+                  className="btn-get-red"
+                >
+                  <span>Get Started Now</span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="call-to-action-center hero-4">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-8">
+              <div className="call-to-action-content">
+                <h3>Want to bring transparency and predictability to your businesses?</h3>
+                <p>Scale your business easily as your business needs grow. <b>Get in touch with our Business Central Experts</b> and reveal a business-centric cloud ERP solution today!</p>
+                <div className="text-center m-o-t new-btn-nn">
+                  <a
+                    data-bs-toggle="modal"
+                    href="#exampleModal"
+                    className="btn-get-started scrollto"
+                  >
+                    <span>Get Started </span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-8 col-md-8">
+                <header className="section-header">
+                <h2>Dynamics 365 Business Central: Benefits and Features </h2>
+                <p><b>Microsoft Dynamics 365 Business Central</b> belongs to the suite of next-gen business management solutions - <Link href="/products/microsoft-dynamics-365/"><a>Dynamics 365</a></Link>. It is robust <b>cloud ERP software</b> that helps small businesses and large international groups efficiently streamline their globally spanned operations and keep informed decision-making better all the time. With its powerful features and seamless integration with other Microsoft products, <b>Business Central</b> is an ideal solution for your SMBs (<Link href="https://blog.dynamicssquare.ca/dynamics-365-business-central-benefits-2023/"><a>small and medium-sized businesses</a></Link>).</p>
+              </header>
+            </div>
+          </div>
+          <div className="row p-lg-6">
+            <div className="col-lg-6">
+              <div className="finace-acc">
+                <div className="accordion" id="accnew">
+                  <div className="accordion-item">
+                    <h3 className="accordion-header" id="headingTwo">
+                      <button
+                        className="accordion-button collapsed"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#fin1"
+                        aria-expanded="false"
+                        aria-controls="fin1"
+                      >
+                        <span>Dynamics 365 Business Central: Features & Their Benefits</span>
+                      </button>
+                    </h3>
+                    <div
+                      id="fin1"
+                      className="accordion-collapse collapse"
+                      aria-labelledby="headingTwo"
+                      data-bs-parent="#accnew"
+                    >
+                      <div className="accordion-body">
+                        <p>Business Central helps you with its comprehensive <Link href="/products/microsoft-dynamics-365-finance/"><a>financial management capabilities</a></Link>, including accounts payable and receivable, general ledger, fixed assets, and cash management.</p>
+                        <p><b>Benefit:</b> You can automate your financial processes, get real-time insights into your financial performance, and make better decisions. </p>
+                      </div>
+                    </div>
+                  </div>
+                  <hr />
+                  <div className="accordion-item">
+                    <h3 className="accordion-header" id="headingTwo">
+                      <button
+                        className="accordion-button collapsed"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#fin2"
+                        aria-expanded="false"
+                        aria-controls="fin2"
+                      >
+                        <span>Business Central Feature: Supply Chain Management</span>
+                      </button>
+                    </h3>
+                    <div
+                      id="fin2"
+                      className="accordion-collapse collapse"
+                      aria-labelledby="headingTwo"
+                      data-bs-parent="#accnew"
+                    >
+                      <div className="accordion-body">
+                        <p><b>Dynamics 365 Business Central</b> enables businesses to manage their <Link href="/products/dynamics-365-supply-chain-management/"><a>supply chain operations</a></Link> efficiently, from procurement to inventory management and production.</p>
+                        <p><b>Benefit:</b> Help your admins optimize your inventory levels, reduce lead times, and improve your supply chain visibility. </p>
+                      </div>
+                    </div>
+                  </div>
+                  <hr />
+                  <div className="accordion-item">
+                    <h3 className="accordion-header" id="headingTwo">
+                      <button
+                        className="accordion-button collapsed"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#fin3"
+                        aria-expanded="false"
+                        aria-controls="fin3"
+                      >
+                        <span>Business Central Feature: Sales and Service Management</span>
+                      </button>
+                    </h3>
+                    <div
+                      id="fin3"
+                      className="accordion-collapse collapse"
+                      aria-labelledby="headingTwo"
+                      data-bs-parent="#accnew"
+                    >
+                      <div className="accordion-body">
+                        <p><b>Microsoft Business Central</b> provides you with a complete set of <Link href="/products/microsoft-dynamics-365-sales/"><a>dynamics 365 sales</a></Link> and <b>service management tools</b>, including <b>customer relationship management (CRM)</b>, sales order processing, service order management, and project management.</p>
+                        <p><b>Benefit:</b> Manage your sales and service processes from end to end, deliver exceptional customer experiences, and drive business growth.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-6">
+              <div className="finace-acc">
+                <hr className="mob-disp" />
+                <div className="accordion" id="accnews">
+                  <div className="accordion-item">
+                    <h3 className="accordion-header" id="headingTwo">
+                      <button
+                        className="accordion-button collapsed"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#fin4"
+                        aria-expanded="false"
+                        aria-controls="fin4"
+                      >
+                        
+                        <span>Business Central Feature: Human Resource Management</span>
+                      </button>
+                    </h3>
+                    <div
+                      id="fin4"
+                      className="accordion-collapse collapse"
+                      aria-labelledby="headingTwo"
+                      data-bs-parent="#accnews"
+                    >
+                      <div className="accordion-body">
+                       <p><b>Dynamics 365 Business Central</b> assists businesses to manage their <Link href="/products/dynamics-365-human-resources/"><a>human resources (HR) operations</a></Link>, from recruiting and onboarding to performance management and payroll.</p>
+                       <p><b>Benefit:</b> Streamline your HR processes, improve employee productivity and engagement, and comply with HR regulations, and other compliances.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <hr />
+                  <div className="accordion-item">
+                    <h3 className="accordion-header" id="headingTwo">
+                      <button
+                        className="accordion-button collapsed"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#fin5"
+                        aria-expanded="false"
+                        aria-controls="fin5"
+                      >
+                        
+                        <span>Business Central Feature: Project Management</span>
+                      </button>
+                    </h3>
+                    <div
+                      id="fin5"
+                      className="accordion-collapse collapse"
+                      aria-labelledby="headingTwo"
+                      data-bs-parent="#accnews"
+                    >
+                      <div className="accordion-body">
+                        <p><b>Microsoft Dynamics 365 Business Central</b> has powerful project management capabilities, including project planning, resource management, time and expense tracking, and project billing.</p>
+                        <p><b>Benefit:</b> You can manage your projects more efficiently, deliver projects on time and within budget, and maximize your project profitability.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+      </section>
+
+      <section>
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-8 col-md-8">
+                <header className="section-header">
+                <h2>Core Benefits of Dynamics 365 Business Central Implementation</h2>
+                <p>Help You Unlock New Levels of Productivity & Customer Engagement with Breeze.</p>
+                <p>Whether you are a <b>small business or a medium-sized enterprise</b>, Business Central can help you achieve your business goals and take your business to the next level better than other <b>cloud enterprise resource planning solutions (ERPs)</b>.</p>
+              </header>
+            </div>
+          </div>
+
+          <div className="row g-5">
+            <div className="col-lg-6">
+            <div className="style-grid-1">
+                <div className="style-grid-content">
+                  <h3>Simplifying Your Business Processes</h3>
+                  <p>You can automate and streamline your core business processes, which reduces manual work and improves the operational efficiency of your plant. With <b>D365 Business Central</b>, you can eliminate redundant tasks, improve collaboration, and focus on driving business growth.</p>
+                </div>
+              </div>
+              <div className="style-grid-1">
+                <div className="style-grid-content">
+                  <h3>Help You Make Informed Decisions</h3>
+                  <p>With real-time insights into your business performance, <b>Dynamics 365 Business Central</b> enables you to quickly make informed decisions. You can access customizable dashboards and reports, track key performance indicators, and identify areas for improvement.</p>
+                </div>
+              </div>
+              <div className="style-grid-1">
+                <div className="style-grid-content">
+                  <h3>Scale Your Business</h3>
+                  <p>Business Central is designed to grow as your business grows. With its flexible architecture and <Link href="/products/microsoft-dynamics-365/"><a>seamless integration with other Microsoft products</a></Link>, this cloud ERP can adapt to your changing business needs and enable you to scale your operations efficiently.</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div className="style-grid-1">
+                <div className="style-grid-content">
+                  <h3>Work Anywhere, Anytime</h3>
+                  <p><Link href="/products/dynamics-365-business-central/"><a>Microsoft Dynamics 365 Business Central</a></Link> is a cloud-based solution that enables you to access your business data and applications from anywhere, anytime. With this user-friendly solution, you can work from home, on the go, or from any device, ensuring that you can always stay connected to your business.</p>
+                </div>
+              </div>
+
+              <div className="style-grid-1">
+                <div className="style-grid-content">
+                  <h3>Enhanced Security and Compliance</h3>
+                  <p><Link href="/products/microsoft-azure/"><a>Microsoft Azure</a></Link>, <b>Microsoft Cloud</b>, and Dataverse power <b>Dynamics 365 Business Central</b> to offer enterprise-grade security and compliance features for your business, ensuring that your business data is secure and compliant with regulatory requirements. You can protect your business data from cyber threats, reduce data loss, and more. </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </section>
+
+
+
+    </>
+  );
+};
+
+export default Finance;
