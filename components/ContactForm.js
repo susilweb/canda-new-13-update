0b5a2ecfaecc6ee44c733a0d839964cfddbbecaa
Sushil Behera <sbehera@mpgbis.com>
2024-04-03 15:35:55 +0530

    update

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,77 +1,117 @@
-import { useState } from 'react';
+// components/ContactForm.js
+import React from 'react';
+import styles from '../styles/ContactForm.module.css'; // Import CSS module
 
 const ContactForm = () => {
-  const [formData, setFormData] = useState({
-    Company: '',
-    'Last Name': ''
-    // Add other form fields here
-  });
-
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const form = e.target;
-    // Perform form submission logic here
-    fetch(form.action, {
-      method: form.method,
-      body: new URLSearchParams(new FormData(form)).toString(),
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
-    })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Failed to submit form');
+  function checkMandatory583330000000339005() {
+    var mndFileds = new Array('Company', 'Last Name');
+    var fldLangVal = new Array('Company', 'Last\x20Name');
+    for (var i = 0; i < mndFileds.length; i++) {
+      var fieldObj = document.forms['WebToLeads583330000000339005'][mndFileds[i]];
+      if (fieldObj) {
+        if (((fieldObj.value).replace(/^\s+|\s+$/g, '')).length == 0) {
+          if (fieldObj.type == 'file') {
+            alert('Please select a file to upload.');
+            fieldObj.focus();
+            return false;
+          }
+          alert(fldLangVal[i] + ' cannot be empty.');
+          fieldObj.focus();
+          return false;
+        } else if (fieldObj.nodeName == 'SELECT') {
+          if (fieldObj.options[fieldObj.selectedIndex].value == '-None-') {
+            alert(fldLangVal[i] + ' cannot be none.');
+            fieldObj.focus();
+            return false;
+          }
+        } else if (fieldObj.type == 'checkbox') {
+          if (fieldObj.checked == false) {
+            alert('Please accept  ' + fldLangVal[i]);
+            fieldObj.focus();
+            return false;
+          }
         }
-        // Handle successful form submission
-        console.log('Form submitted successfully');
-        // Reset form fields
-        form.reset(); // Use form.reset() instead of s.reset()
-      })
-      .catch(error => {
-        // Handle form submission error
-        console.error('Form submission error:', error);
-      });
-  };
-  
+        try {
+          if (fieldObj.name == 'Last Name') {
+            name = fieldObj.value;
+          }
+        } catch (e) {}
+      }
+    }
+    var urlparams = new URLSearchParams(window.location.search);
+    if (urlparams.has('service') && (urlparams.get('service') === 'smarturl')) {
+      var webform = document.getElementById('webform583330000000339005');
+      var service = urlparams.get('service');
+      var smarturlfield = document.createElement('input');
+      smarturlfield.setAttribute('type', 'hidden');
+      smarturlfield.setAttribute('value', service);
+      smarturlfield.setAttribute('name', 'service');
+      webform.appendChild(smarturlfield);
+    }
+    document.querySelector('.crmWebToEntityForm .formsubmit').setAttribute('disabled', true);
+  }
+
+  function tooltipShow583330000000339005(el) {
+    var tooltip = el.nextElementSibling;
+    var tooltipDisplay = tooltip.style.display;
+    if (tooltipDisplay == 'none') {
+      var allTooltip = document.getElementsByClassName('zcwf_tooltip_over');
+      for (var i = 0; i < allTooltip.length; i++) {
+        allTooltip[i].style.display = 'none';
+      }
+      tooltip.style.display = 'block';
+    } else {
+      tooltip.style.display = 'none';
+    }
+  }
 
   return (
-    <div id='crmWebToEntityForm' className='zcwf_lblLeft crmWebToEntityForm' style={{ backgroundColor: 'white', color: 'black', maxWidth: '600px' }}>
-      <form id='webform583330000000339005' action='https://crm.zoho.in/crm/WebToLeadForm' name='WebToLeads583330000000339005' method='POST' onSubmit={handleSubmit} acceptCharset='UTF-8'>
-        <input type='text' style={{ display: 'none' }} name='xnQsjsdp' value='659b31daccca0c70473967b130eed38edab0b4b17f03cbd2c77dedec7664fdd0' />
+    <div id='crmWebToEntityForm' className={`${styles.crmWebToEntityForm} ${styles.zcwf_lblLeft}`}>
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      <meta http-equiv='content-type' content='text/html;charset=UTF-8' />
+
+      <form
+        id='webform583330000000339005'
+        action='https://crm.zoho.in/crm/WebToLeadForm'
+        name='WebToLeads583330000000339005'
+        method='POST'
+        onSubmit='javascript:document.charset="UTF-8"; return checkMandatory583330000000339005()'
+        acceptCharset='UTF-8'
+      >
+        <input type='text' style={{ display: 'none' }} name='xnQsjsdp' value='301390302ac5208fa9c9d258747757f5410edba55580cadaf090cd53cfaa1cba' />
         <input type='hidden' name='zc_gad' id='zc_gad' value='' />
-        <input type='text' style={{ display: 'none' }} name='xmIwtLD' value='a055472592c0ec7bb3c2d3a72c9dce78262a67da270f19b8193c3a7ededd4a4d63a61322c97ffbabefcaff26cf324c51' />
+        <input type='text' style={{ display: 'none' }} name='xmIwtLD' value='91c9cb0353618abac9c19863620775ef36911c41096cd10529d54bc8f30e515106c89f9d2ad10417c9ee0b46566b7a78' />
         <input type='text' style={{ display: 'none' }} name='actionType' value='TGVhZHM=' />
-        <input type='text' style={{ display: 'none' }} name='returnURL' value='null' />
+        <input type='text' style={{ display: 'none' }} name='returnURL' value='https://canda-new-13-update.vercel.app/thank-you/' />
+        {/* Do not remove this code. */}
+        <div className='zcwf_title'>contact us</div>
         <div className='zcwf_row'>
-          <div className='zcwf_col_lab' style={{ fontSize: '12px', fontFamily: 'Arial' }}>
-            <label htmlFor='Company'>Company<span style={{ color: 'red' }}>*</span></label>
+          <div className='zcwf_col_lab'>
+            <label htmlFor='Company'>
+              Company<span style={{ color: 'red' }}>*</span>
+            </label>
           </div>
           <div className='zcwf_col_fld'>
-            <input type='text' id='Company' name='Company' value={formData.Company} onChange={handleChange} maxLength='200' />
+            <input type='text' id='Company' name='Company' maxLength='200' />
             <div className='zcwf_col_help'></div>
           </div>
         </div>
         <div className='zcwf_row'>
-          <div className='zcwf_col_lab' style={{ fontSize: '12px', fontFamily: 'Arial' }}>
-            <label htmlFor='Last_Name'>Last Name<span style={{ color: 'red' }}>*</span></label>
+          <div className='zcwf_col_lab'>
+            <label htmlFor='Last_Name'>
+              Last Name<span style={{ color: 'red' }}>*</span>
+            </label>
           </div>
           <div className='zcwf_col_fld'>
-            <input type='text' id='Last_Name' name='Last Name' value={formData['Last Name']} onChange={handleChange} maxLength='80' />
+            <input type='text' id='Last_Name' name='Last Name' maxLength='80' />
             <div className='zcwf_col_help'></div>
           </div>
         </div>
         <div className='zcwf_row'>
           <div className='zcwf_col_lab'></div>
           <div className='zcwf_col_fld'>
-            <input type='submit' id='formsubmit' className='formsubmit zcwf_button' value='Submit' title='Submit' />
-            <input type='reset' className='zcwf_button' name='reset' value='Reset' title='Reset' />
+            <input type='submit' id='formsubmit' className={`formsubmit ${styles.zcwf_button}`} value='Submit' title='Submit' />
+            <input type='reset' className={styles.zcwf_button} name='reset' value='Reset' title='Reset' />
           </div>
         </div>
       </form>
